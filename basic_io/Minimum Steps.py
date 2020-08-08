cb248ed48a7a79d751e9ab9bdefffb9cc23bcb98
Long Vũ ✔ <banhmy1290@gmail.com>
2020-08-08 19:05:22 +0700

    add

---
+++ basic_io/Minimum Steps.py
@@ -0,0 +1,16 @@
+from math import ceil
+
+
+def find_min(a, b, c):
+    if a >= b:
+        return ceil((a-b) / 2)
+    if b % c == 0:
+        return 1 + find_min(a, b // c, c)
+    else:
+        x = (b//c + 1) * c
+        return (ceil((x-b) / 2) + find_min(a, x, c))
+
+
+for i in range(int(input())):
+    k, m, n = map(int, input().split())
+    print(find_min(k, m, n))
